bb3fade40ca183e42bb82a6e26c24ac23557933a
mrktptn <marktipton95@gmail.com>
2023-12-07 16:47:54 -0600

    first draft of js for amenity checkboxes. Not functional yet

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+$(document).ready(function() {
+  let amenityIds = [];
+
+  function updateAmenities() {
+    const amenitiesH4 = $('#amenities h4');
+
+    amenitiesH4.text(amenityIds.join(', '));
+  }
+
+  $('input[type="checkbox"]').change(function() {
+    const id = $(this).data('id');
+
+    if ($(this).prop('checked')) {
+      amenityIds.push(amenityId);
+    } else {
+      amenityIds = amenityIds.filter(function(id) {
+        return id !== amenityId;
+      });
+    }
+
+    updateAmenities();
+  });
+});
