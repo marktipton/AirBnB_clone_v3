e8f2acc5a6474c2407e3899acbe81a124182a958
mrktptn <marktipton95@gmail.com>
2023-12-07 16:54:16 -0600

    fixed semistandard but still not updating amenities correctly

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
-$(document).ready(function() {
+$(document).ready(function () {
   let amenityIds = [];
 
-  function updateAmenities() {
+  function updateAmenities () {
     const amenitiesH4 = $('#amenities h4');
 
     amenitiesH4.text(amenityIds.join(', '));
   }
 
-  $('input[type="checkbox"]').change(function() {
-    const id = $(this).data('id');
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
 
     if ($(this).prop('checked')) {
       amenityIds.push(amenityId);
     } else {
-      amenityIds = amenityIds.filter(function(id) {
+      amenityIds = amenityIds.filter(function (id) {
         return id !== amenityId;
       });
     }
