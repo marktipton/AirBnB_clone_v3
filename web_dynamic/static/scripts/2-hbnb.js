8268b17bd781fe040364802e66c0ca07a8832f6a
mrktptn <marktipton95@gmail.com>
2023-12-10 14:05:00 -0600

    made js file for status, added available class to 3-header.css

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+  let checkedAmenityIds = {};
+
+  function updateAmenities () {
+    const amenitiesH4 = $('#amenities_h4');
+    const amenitiesString = Object.values(checkedAmenityIds).join(', ');
+    amenitiesH4.text(amenitiesString);
+  }
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+    if ($(this).prop('checked')) {
+      // if checked, add amenityId to checked Amenities list
+      checkedAmenityIds[amenityId] = amenityName;
+    } else {
+      delete checkedAmenityIds[amenityId];
+    }
+    updateAmenities();
+    console.log(checkedAmenityIds);
+  });
+
+});
